511a8220d49033995f5fcfa9677c8e8df6968dc0
aaron weiner <aaronjacobweiner@yahoo.com>
2021-02-12 18:46:32 -0500

    added some scrript

--- script.js
+++ script.js
@@ -1,62 +1,10 @@
-var question = [
-    {
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-    },{
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-        
-    },{
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-    },{
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-    },{
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-    },{
-        questions: "iksfahsdfn",
-        answers: [
-            {text: "kjsdbf", correct: true},
-            {text: "kjsdfk", correct: false},
-            {text: "kjsdnb", correct: false},
-            {text: "kjsdfd", correct: false}
-        ]
-    }
-]
 
 var timeEl = document.querySelector("#timer");
 var start = document.querySelector("#start-btn");
 var qContainer = document.querySelector("#questions-container");
 var shuffledQuestions, currentQuestionIndex;
 var questionElement = document.getElementById("questions");
-var answersButtons = document.querySelector(".answers");
+var answersButtonElement = document.getElementById("answer-buttons");
 var secondsLeft = 100;
 
 function setTime() {
@@ -67,8 +15,6 @@ function setTime() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      
-      sendMessage();
     }
 
   }, 1000);
@@ -81,21 +27,37 @@ startButton.addEventListener("click", startGame)
 
 function startGame() {
     startButton.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random - .5);
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     qContainer.classList.remove("hide");
-    setNextQuestions()
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+   
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion() {
-    questionElement.innerText = questions.questions
+function showQuestion(question, answers) {
+    questionElement.innerText  = question.question;
+    question.answers.forEach(answer =>{
+      var button = document.createElement("button")
+      buttton.innerText = answer.text
+      button.classList.add("btn")
+    })
 }
 
 function selectAnswer() {
-
+  
 }
-
+var questions = [
+    {
+         question: "What is a boolean?",
+         answers: [
+             {text: "A True or False Statement", correct: true},
+             {text: "A Variable", correct: false},
+             {text: "A Number", correct: false},
+             {text: "A Function", correct: false}
+         ]
+     }
+ ]
