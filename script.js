d7132a53cccfdd3c907a62df19345c2e06b065f3
aaron weiner <aaronjacobweiner@yahoo.com>
2021-02-17 15:00:54 -0500

    fixed some stuff with my scores being printed

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ var timerInterval;
 var backCorrect = document.querySelector("body");
 var scoreTime = document.querySelector("#score-time");
 var score = localStorage.getItem("score");
-
+var highScoreLeftCrn = document.querySelector("#high-score");
+var inputSection = document.querySelector("#input-section");
+var h2 = document.querySelector("h2");
 
 
 var answer1 = document.querySelector("#one");
@@ -25,27 +27,23 @@ var arrOfAnswers = [answer1, answer2, answer3, answer4];
 
 
 function setTime() {  
-    timerInterval = setInterval(function() {
+  timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent =  "Seconds Left: " + secondsLeft;
-
+    
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
     };
     if(secondsLeft === 0){
-      qContainer.classList.add("hide");
       highScoresList.classList.remove("hide");
       retakeQuizBtn.classList.remove("hide");
+      qContainer.classList.add("hide");
+      highScoreLeftCrn.classList.add("hide");
+      timeEl.classList.add("hide");
       viewHighScores()
     };
   }, 1000);
 }
-
-
-start.addEventListener("click", init);
-retakeQuizBtn.addEventListener("click", retakeQuiz);
-goBackBtn.addEventListener("click", goBack);
-
 
 arrOfAnswers.forEach(btn => {
   btn.addEventListener("click", selectAnswer)
@@ -56,21 +54,31 @@ function init() {
   startGame();
 }
 
+retakeQuizBtn.addEventListener("click", retakeQuiz);
+
 function retakeQuiz() {
   location.reload();
 }
+
+goBackBtn.addEventListener("click", goBack);
 
 function goBack() {
   location.reload();
 }
 
+start.addEventListener("click", init);
+
 function startGame() {
-    start.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    qContainer.classList.remove("hide");
-    setNextQuestion();
+  start.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  qContainer.classList.remove("hide");
+  setNextQuestion();
 }
+
+  // Line 84 through 117 have to do with my questions and answers
+    // Questions sections 
+
 
 function setNextQuestion() {
   if(currentQuestionIndex < questions.length){
@@ -81,6 +89,7 @@ function setNextQuestion() {
     scoreTime.textContent = score;
     retakeQuizBtn.classList.remove("hide");
     goBackBtn.classList.add("hide");
+    timeEl.classList.add("hide");
   }
 }
 
@@ -92,7 +101,7 @@ function showQuestion(question) {
   }
 }
 
-
+    // Answer selection function 
 
 function selectAnswer(e) {
   if(e.target.dataset.correct === "true"){    
@@ -111,56 +120,117 @@ function correctColor() {
   answersButtonElement.setAttribute("style", "background-color: green;");
 }
 
-
+    // Functions that have to do with my high score sections
 
 var highScores = document.getElementById("high-score");
-highScores.addEventListener("click", viewHighScores);
-highScoresList = document.getElementById("high-score-box");
+highScores.addEventListener("click", highScoreButton);
 
-function viewHighScores() {
-    start.classList.add("hide");
-    qContainer.classList.add("hide");
-    highScoresList.classList.remove("hide");
-    goBackBtn.classList.remove("hide");
-    score = secondsLeft;
-    localStorage.setItem("score", score);
+function highScoreButton() {
+  start.classList.add("hide");
+  qContainer.classList.add("hide");
+  inputSection.classList.add("hide");
+  retakeQuizBtn.classList.add("hide");
+  h2.classList.add("hide");
+  goBackBtn.classList.remove("hide");
+  highScoresList.classList.remove("hide");
 }
 
 
+var highScoresList = document.getElementById("high-score-box");
+
+function viewHighScores() {
+  start.classList.add("hide");
+  qContainer.classList.add("hide");
+  highScoresList.classList.remove("hide");
+  goBackBtn.classList.remove("hide");
+  score = secondsLeft;
+  localStorage.setItem("score", score);
+}
+
+
+// High Score inputting and storing
+
+var nameInput = document.querySelector("#name");
+var scoresArray = [];
+var listedScores = document.querySelector("#list-scores-name");
+var submitScore = document.querySelector("#submit-score");
+
+
+
+function initScores() {
+  var storedScores = JSON.parse(localStorage.getItem("scoresArray"));
+
+  if(storedScores !== null){
+    scoresArray = storedScores;
+  }
+  renderScores();
+}
+
+function renderScores() {
+  listedScores.innerHTML= "";
+  
+  for(var i = 0; i < scoresArray.length; i++){
+    var scoreItem = scoresArray[i];
+    
+    var li = document.createElement("li");
+    li.textContent = scoreItem;
+    li.setAttribute("data-index", i); 
+    
+    listedScores.appendChild(li);    
+  }
+}
+
+function storeScores() {
+  localStorage.setItem("scoresArray", JSON.stringify(scoresArray));
+}
+
+submitScore.addEventListener("click", function(event){
+  event.preventDefault();
+  var scoreText = nameInput.value.trim();
+  var concatScore = scoreText.concat(score);
+  scoresArray.push(concatScore);
+  nameInput.value = "";
+  
+  storeScores();
+  renderScores();
+});
+initScores()
+
+
+// Object of questions and answers
 
 var questions = [
-  {
-    question: "What is a boolean?",
-    answers: [
-      { text: "A True or False Statement", correct: true },
-      { text: "A Variable", correct: false },
-      { text: "A Number", correct: false },
-      { text: "A Function", correct: false }
-    ]
-  }, {
-    question: "What is a String?",
-    answers: [
-      { text: "A True or False Statement", correct: true },
-      { text: "A Variable", correct: false },
-      { text: "A Number", correct: false },
-      { text: "A Function", correct: false }
-    ]
-  }, {
-    question: "What is a Undefined Variable?",
-    answers: [
-      { text: "A True or False Statement", correct: true },
-      { text: "A Variable", correct: false },
-      { text: "A Number", correct: false },
-      { text: "A Function", correct: false }
-    ]
-  }, {
-    question: "How do you grab an ID?",
-    answers: [
-      { text: "A True or False Statement", correct: true },
-      { text: "A Variable", correct: false },
-      { text: "A Number", correct: false },
-      { text: "A Function", correct: false }
-    ]
-  }
+{
+question: "What is a boolean?",
+answers: [
+  { text: "A True or False Statement", correct: true },
+  { text: "A Variable", correct: false },
+  { text: "A Number", correct: false },
+  { text: "A Function", correct: false }
 ]
-
+}, {
+question: "What is a String?",
+answers: [
+  { text: "A True or False Statement", correct: true },
+  { text: "A Variable", correct: false },
+  { text: "A Number", correct: false },
+  { text: "A Function", correct: false }
+]
+}, {
+question: "What is a Undefined Variable?",
+answers: [
+  { text: "A True or False Statement", correct: true },
+  { text: "A Variable", correct: false },
+  { text: "A Number", correct: false },
+  { text: "A Function", correct: false }
+]
+}, {
+question: "How do you grab an ID?",
+answers: [
+  { text: "A True or False Statement", correct: true },
+  { text: "A Variable", correct: false },
+  { text: "A Number", correct: false },
+  { text: "A Function", correct: false }
+]
+}
+]
