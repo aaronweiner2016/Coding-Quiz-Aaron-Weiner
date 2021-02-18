eba066d69d62321b31bf167ad2ef12de6e3cb782
aaron weiner <aaronjacobweiner@yahoo.com>
2021-02-18 15:06:48 -0500

    adding readme text

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var timeEl = document.querySelector("#timer");
 var start = document.querySelector("#start-btn");
+var next = document.querySelector("#next");
 var qContainer = document.querySelector("#questions-container");
 var shuffledQuestions, currentQuestionIndex;
 var questionElement = document.getElementById("questions");
@@ -15,7 +16,12 @@ var score = localStorage.getItem("score");
 var highScoreLeftCrn = document.querySelector("#high-score");
 var inputSection = document.querySelector("#input-section");
 var h2 = document.querySelector("h2");
+var incorrectAlert = document.querySelector("#wrong");
+var correctAlert = document.querySelector("#right");
+var gameOver = document.querySelector("#game-over");
+var ScoreZero = document.querySelector("#score-zero")
 
+// create eventlistener for button, hide green in this
 
 var answer1 = document.querySelector("#one");
 var answer2 = document.querySelector("#two");
@@ -26,15 +32,13 @@ var arrOfAnswers = [answer1, answer2, answer3, answer4];
 
 
 
-function setTime() {  
-  timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent =  "Seconds Left: " + secondsLeft;
-    
-    if(secondsLeft === 0) {
+function setTime() {      //starts the time interval
+  timerInterval = setInterval(function () {
+    secondsLeft--;   // - one second
+    timeEl.textContent = "Seconds Left: " + secondsLeft;
+
+    if (secondsLeft <= 0) {
       clearInterval(timerInterval);
-    };
-    if(secondsLeft === 0){
       highScoresList.classList.remove("hide");
       retakeQuizBtn.classList.remove("hide");
       qContainer.classList.add("hide");
@@ -69,6 +73,7 @@ function goBack() {
 start.addEventListener("click", init);
 
 function startGame() {
+  // next.classList.remove("hide");
   start.classList.add("hide");
   shuffledQuestions = questions.sort(() => Math.random() - .5);
   currentQuestionIndex = 0;
@@ -76,43 +81,64 @@ function startGame() {
   setNextQuestion();
 }
 
-  // Line 84 through 117 have to do with my questions and answers
-    // Questions sections 
+// Line 84 through 117 have to do with my questions and answers
+// Questions sections 
 
 
 function setNextQuestion() {
-  if(currentQuestionIndex < questions.length){
+  if (currentQuestionIndex < questions.length) {
     showQuestion(shuffledQuestions[currentQuestionIndex])
-  }else{
-    clearInterval(timerInterval)
-    viewHighScores()
-    scoreTime.textContent = score;
-    retakeQuizBtn.classList.remove("hide");
-    goBackBtn.classList.add("hide");
-    timeEl.classList.add("hide");
+  } else {
+    timeEl.textContent = "Seconds Left: " + secondsLeft;
+    clearInterval(timerInterval);
+    gameOver.classList.remove("hide");
+    setTimeout(highScoresAfter,2000)
+    qContainer.classList.add("hide");
+    incorrectAlert.classList.add("hide");
+    correctAlert.classList.add("hide");
   }
 }
 
+
+function highScoresAfter() {
+  viewHighScores()
+    scoreTime.textContent = score;
+    retakeQuizBtn.classList.remove("hide");
+    goBackBtn.classList.add("hide");
+    gameOver.classList.add("hide");
+}
+
 function showQuestion(question) {
-  questionElement.innerText  = question.question;
-  for (var i = 0; i < arrOfAnswers.length; i++){
+  questionElement.innerText = question.question;
+  for (var i = 0; i < arrOfAnswers.length; i++) {
     arrOfAnswers[i].innerText = question.answers[i].text
     arrOfAnswers[i].setAttribute("data-correct", question.answers[i].correct)
   }
 }
 
-    // Answer selection function 
+// Answer selection function 
+
+next.addEventListener("click", nextQ);
+
+function nextQ() {
+  currentQuestionIndex++;
+  setNextQuestion()
+  backCorrect.setAttribute("style", "background-color: cornflowerblue;")
+  next.classList.add("hide")
+  incorrectAlert.classList.add("hide");
+  correctAlert.classList.add("hide");
+}
 
 function selectAnswer(e) {
-  if(e.target.dataset.correct === "true"){    
+  if (e.target.dataset.correct === "true") {
     backCorrect.setAttribute("style", "background-color: green;")
-    currentQuestionIndex++;
-    setNextQuestion()        
-  }else{
+    next.classList.remove("hide");
+    correctAlert.classList.remove("hide");      
+  } else {
     backCorrect.setAttribute("style", "background-color: red;")
     secondsLeft = secondsLeft - 10;
-    currentQuestionIndex++;
-    setNextQuestion()
+    next.classList.remove("hide");
+    incorrectAlert.classList.remove("hide");
   }
 }
 
@@ -120,7 +146,7 @@ function correctColor() {
   answersButtonElement.setAttribute("style", "background-color: green;");
 }
 
-    // Functions that have to do with my high score sections
+// Functions that have to do with my high score sections
 
 var highScores = document.getElementById("high-score");
 highScores.addEventListener("click", highScoreButton);
@@ -131,8 +157,13 @@ function highScoreButton() {
   inputSection.classList.add("hide");
   retakeQuizBtn.classList.add("hide");
   h2.classList.add("hide");
+  incorrectAlert.classList.add("hide");
+  correctAlert.classList.add("hide");
+  next.classList.add("hide");
   goBackBtn.classList.remove("hide");
   highScoresList.classList.remove("hide");
+  gameOver.classList.add("hide");
+  clearInterval(timerInterval);
 }
 
 
@@ -141,6 +172,9 @@ var highScoresList = document.getElementById("high-score-box");
 function viewHighScores() {
   start.classList.add("hide");
   qContainer.classList.add("hide");
+  incorrectAlert.classList.add("hide");
+  correctAlert.classList.add("hide");
+  ScoreZero.classList.remove("hide");
   highScoresList.classList.remove("hide");
   goBackBtn.classList.remove("hide");
   score = secondsLeft;
@@ -155,44 +189,58 @@ var scoresArray = [];
 var listedScores = document.querySelector("#list-scores-name");
 var submitScore = document.querySelector("#submit-score");
 
+var separator = ["   "];
 
 
 function initScores() {
   var storedScores = JSON.parse(localStorage.getItem("scoresArray"));
 
-  if(storedScores !== null){
+  if (storedScores !== null) {
     scoresArray = storedScores;
   }
   renderScores();
 }
 
 function renderScores() {
-  listedScores.innerHTML= "";
-  
-  for(var i = 0; i < scoresArray.length; i++){
+  listedScores.innerHTML = "";
+
+  for (var i = 0; i < scoresArray.length; i++) {
     var scoreItem = scoresArray[i];
-    
+
     var li = document.createElement("li");
     li.textContent = scoreItem;
-    li.setAttribute("data-index", i); 
-    
-    listedScores.appendChild(li);    
+    li.setAttribute("data-index", i);
+
+    listedScores.appendChild(li);
   }
 }
+
+// function myFunction() {
+//   scoresArray.sort(function(a, b){return a-b});
+// }
 
 function storeScores() {
   localStorage.setItem("scoresArray", JSON.stringify(scoresArray));
 }
 
-submitScore.addEventListener("click", function(event){
+// function disableButton(submitScore){
+//   document.getElementById(submitScore).disabled = true;
+//   alert("Button has been disabled.");
+// }
+
+submitScore.addEventListener("click", function (event) {
   event.preventDefault();
   var scoreText = nameInput.value.trim();
-  var concatScore = scoreText.concat(score);
+  var concatScore = scoreText.concat(separator, score);
   scoresArray.push(concatScore);
-  nameInput.value = "";
-  
+  // nameInput.value = "";
+
+
+
   storeScores();
   renderScores();
+  // myFunction();
+  // disableButton(submitScore);
 });
 initScores()
 
@@ -200,37 +248,69 @@ initScores()
 // Object of questions and answers
 
 var questions = [
-{
-question: "What is a boolean?",
-answers: [
-  { text: "A True or False Statement", correct: true },
-  { text: "A Variable", correct: false },
-  { text: "A Number", correct: false },
-  { text: "A Function", correct: false }
-]
-}, {
-question: "What is a String?",
-answers: [
-  { text: "A True or False Statement", correct: true },
-  { text: "A Variable", correct: false },
-  { text: "A Number", correct: false },
-  { text: "A Function", correct: false }
-]
-}, {
-question: "What is a Undefined Variable?",
-answers: [
-  { text: "A True or False Statement", correct: true },
-  { text: "A Variable", correct: false },
-  { text: "A Number", correct: false },
-  { text: "A Function", correct: false }
-]
-}, {
-question: "How do you grab an ID?",
-answers: [
-  { text: "A True or False Statement", correct: true },
-  { text: "A Variable", correct: false },
-  { text: "A Number", correct: false },
-  { text: "A Function", correct: false }
-]
-}
+  {
+    question: "What is a boolean?",
+    answers: [
+      { text: "A True or False Statement", correct: true },
+      { text: "A Variable", correct: false },
+      { text: "A Number", correct: false },
+      { text: "A Function", correct: false }
+    ]
+  }, {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<scripting>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<js>", correct: false }
+    ]
+  }, {
+    question: "What is a Undefined Variable?",
+    answers: [
+      { text: "A True or False Statement", correct: true },
+      { text: "A Variable", correct: false },
+      { text: "A Number", correct: false },
+      { text: "A Function", correct: false }
+    ]
+  }//, {
+  //   question: "How do you grab an ID?",
+  //   answers: [
+  //     { text: "A True or False Statement", correct: true },
+  //     { text: "A Variable", correct: false },
+  //     { text: "A Number", correct: false },
+  //     { text: "A Function", correct: false }
+  //   ]
+  // }, {
+  //   question: "How do you grab an ID?",
+  //   answers: [
+  //     { text: "A True or False Statement", correct: true },
+  //     { text: "A Variable", correct: false },
+  //     { text: "A Number", correct: false },
+  //     { text: "A Function", correct: false }
+  //   ]
+  // }, {
+  //   question: "How do you grab an ID?",
+  //   answers: [
+  //     { text: "A True or False Statement", correct: true },
+  //     { text: "A Variable", correct: false },
+  //     { text: "A Number", correct: false },
+  //     { text: "A Function", correct: false }
+  //   ]
+  // }, {
+  //   question: "How do you grab an ID?",
+  //   answers: [
+  //     { text: "A True or False Statement", correct: true },
+  //     { text: "A Variable", correct: false },
+  //     { text: "A Number", correct: false },
+  //     { text: "A Function", correct: false }
+  //   ]
+  // }, {
+  //   question: "How do you grab an ID?",
+  //   answers: [
+  //     { text: "A True or False Statement", correct: true },
+  //     { text: "A Variable", correct: false },
+  //     { text: "A Number", correct: false },
+  //     { text: "A Function", correct: false }
+  //   ]
+  // }
 ]
